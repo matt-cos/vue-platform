e1125e222a061e3c3f65e027daff0b8546f85466
Matt Cosentino <matt@learnedmedia.com>
2018-08-01 22:45:49 -0400

    updated endpoint name for home page's content

--- client/src/services/PostsService.js
+++ client/src/services/PostsService.js
@@ -2,7 +2,7 @@ import Api from '@/services/Api'
 
 export default {
   fetchHome () {
-    return Api().get('')
+    return Api().get('home-content')
   },
   fetchPosts () {
     return Api().get('posts')
--- server/src/app.js
+++ server/src/app.js
@@ -8,7 +8,7 @@ app.use(morgan('combined'))
 app.use(bodyParser.json())
 app.use(cors())
 
-app.get('/', (req, res) => {
+app.get('/home-content', (req, res) => {
   res.send(
     [{
       title: "Matt's Vue Platform",
